8cf3e9990835f67bac2553b5f0fc6ea7433e9c4a
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2023-07-05 10:29:51 -0700

    Implemented parallel processing using multiple workers

--- detectionai/main.py
+++ detectionai/main.py
@@ -1,11 +1,14 @@
 import face_recognition
 import cv2
+from multiprocessing import Process, Manager, cpu_count, set_start_method
+import time
 import numpy as np
+import os
 
-# Set up paths
-dataset_dir = './data/dataset/'  # Directory containing the training dataset
+# Define the dataset directory
+dataset_dir = './data/people'
 
-# Load the dataset of known faces
+# Load known faces and labels from the dataset directory
 known_faces = []
 known_labels = []
 for label_name in os.listdir(dataset_dir):
@@ -18,59 +21,107 @@ for label_name in os.listdir(dataset_dir):
             known_faces.append(face_encoding)
             known_labels.append(label_name)
 
-# Initialize video capture
-video_capture = cv2.VideoCapture(0)
+# Initialize shared variables for inter-process communication
+manager = Manager()
+read_frame_list = manager.list([None] * cpu_count())  # List to store frames for reading
+write_frame_list = manager.list([None] * cpu_count())  # List to store frames for writing
+frame_delay = 0.01  # Delay between frames for smoother video
+is_exit = False  # Flag to indicate if the program should exit
+buff_num = cpu_count()  # Buffer index for reading frames
+read_num = cpu_count()  # Frame index for reading frames
+write_num = 1  # Frame index for writing frames
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    
-    # Resize frame to improve performance
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    
-    # Convert the image from BGR color (OpenCV) to RGB color (face_recognition)
-    rgb_small_frame = small_frame[:, :, ::-1]
-    
-    # Find all the faces and their encodings in the current frame
-    face_locations = face_recognition.face_locations(rgb_small_frame)
-    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    
-    # Initialize an empty list to store the names of verified faces
-    verified_names = []
-    
-    for face_encoding in face_encodings:
-        # Compare the face encoding with the known faces
-        matches = face_recognition.compare_faces(known_faces, face_encoding)
-        name = "Unauthorized"
-        
-        if True in matches:
-            # Face recognized as one of the known faces
-            match_index = matches.index(True)
-            name = known_labels[match_index]
-            verified_names.append(name)
-    
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, verified_names):
-        # Scale back up the face locations since the frame was resized
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-        
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-        
-        # Display the name
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name + ' Verified', (left + 6, bottom - 6), font, 0.5, (0, 255, 0), 1)
-    
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-    
-    # Quit if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Set up the capture process
+def capture(read_frame_list, is_exit, buff_num):
+    video_capture = cv2.VideoCapture(0)
+    while not is_exit.value:
+        if buff_num.value != read_num.value:
+            ret, frame = video_capture.read()
+            read_frame_list[buff_num.value] = frame
+            buff_num.value = next_id(buff_num.value, cpu_count())
+        else:
+            time.sleep(0.01)
+    video_capture.release()
 
-# Release video capture
-video_capture.release()
-cv2.destroyAllWindows()
+# Set up the processing processes
+def process(worker_id, read_frame_list, write_frame_list, is_exit, read_num, buff_num, write_num):
+    while not is_exit.value:
+        while read_num.value != worker_id or read_num.value != prev_id(buff_num.value, cpu_count()):
+            if is_exit.value:
+                break
+            time.sleep(0.01)
+
+        time.sleep(frame_delay)
+        frame_process = read_frame_list[worker_id]
+        read_num.value = next_id(read_num.value, cpu_count())
+
+        rgb_frame = frame_process[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_frame)
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_faces, face_encoding)
+            name = "Unknown"
+            face_distances = face_recognition.face_distance(known_faces, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_labels[best_match_index]
+            face_names.append(name)
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            cv2.rectangle(frame_process, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame_process, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame_process, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        while write_num != worker_id:
+            time.sleep(0.01)
+
+        write_frame_list[worker_id] = frame_process
+        write_num = next_id(write_num, cpu_count())
+
+def next_id(current_id, worker_num):
+    if current_id == worker_num:
+        return 1
+    else:
+        return current_id + 1
+
+def prev_id(current_id, worker_num):
+    if current_id == 1:
+        return worker_num
+    else:
+        return current_id - 1
+
+if __name__ == '__main__':
+    set_start_method('spawn')
+
+    # Start the capture process
+    capture_process = Process(target=capture, args=(read_frame_list, is_exit, buff_num))
+    capture_process.start()
+
+    # Start the processing processes
+    process_list = []
+    for i in range(1, cpu_count() + 1):
+        process_list.append(Process(target=process, args=(i, read_frame_list, write_frame_list, is_exit, read_num, buff_num, write_num)))
+        process_list[-1].start()
+
+    # Initialize the video capture
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        frame = write_frame_list[cpu_count()]
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            is_exit = True
+            break
+
+    video_capture.release()
+
+    # Wait for the processes to finish
+    capture_process.join()
+    for process in process_list:
+        process.join()
+
+    cv2.destroyAllWindows()
